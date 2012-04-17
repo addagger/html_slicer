423db5def33fcfc31ee8b3638972801f97b717da
Valery Kvon <addagger@gmail.com>
2012-04-18 02:23:38 +0400

    resizing the style= attribute (css width/height)

--- lib/html_slicer/resizing.rb
+++ lib/html_slicer/resizing.rb
@@ -24,15 +24,58 @@ module HtmlSlicer
       able_to?(node, @options)
     end
 
+    def width(node)
+      values = []
+      if block_given?
+        node.attributes["width"] = yield
+      else
+        values << absolute_resolution(node.attributes["width"])
+      end
+      if style = node.attributes["style"]
+        style.gsub!(/width:\s+\d+px;/) do |t|
+          t.gsub(/\d+/) do |w|
+            if block_given?
+              yield
+            else
+              values << w.to_i
+              w
+            end
+          end
+        end
+      end
+      values.compact.min
+    end
+
+    def height(node)
+      values = []
+      if block_given?
+        node.attributes["height"] = yield
+      else
+        values << absolute_resolution(node.attributes["height"])
+      end
+      if style = node.attributes["style"]
+        style.gsub!(/height:\s+\d+px;/) do |t|
+          t.gsub(/\d+/) do |h|
+            if block_given?
+              yield
+            else
+              values << h.to_i
+              h
+            end
+          end
+        end
+      end
+      values.compact.min
+    end
+
     def resize!(node)
-      parent_width = node.parent.is_a?(HTML::Tag) ? absolute_resolution(node.parent.attributes["width"]) : nil
-      target_width = parent_width.present? ? parent_width.to_i : @options.width
-      if target_width.present? && node_width = absolute_resolution(node.attributes["width"])
-        node_height = absolute_resolution(node.attributes["height"])
+      target_width = node.parent.is_a?(HTML::Tag) ? width(node.parent)||@options.width : nil
+      if target_width.present? && node_width = width(node)
+        node_height = height(node)
         if node_width > target_width
           ratio = node_width.to_f/target_width
-          node.attributes["width"] = target_width.to_s
-          node.attributes["height"] = (node_height/ratio).round.to_s if node_height
+          width(node) { target_width.to_s }
+          height(node) { (node_height/ratio).round.to_s } if node_height
         end
       end
     end
